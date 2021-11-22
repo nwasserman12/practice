b2fb70d69a81617a44c63b062d4ec6701e91643a
nwasserman12 <nickyann0823@aol.com>
2021-11-22 15:10:53 -0600

    User model created

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,9 +1,64 @@
 package com.example.demo.models;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User extends AbstractEntity{
 
+    @NotNull
+    private String firstName;
 
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String dateOfBirth;
+
+    @NotNull
+    @Size(max=15, message= "Username must be under 15 characters.")
+    private String username;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private String verifyPassword;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
 }
