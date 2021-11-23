efb63f78324b23e9dfaddaac8ab0170f932d16da
nwasserman12 <nickyann0823@aol.com>
2021-11-23 13:51:10 -0600

    User repository created

--- src/main/java/com/example/demo/data/UserRepository.java
+++ src/main/java/com/example/demo/data/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.data;
 
-public interface UserRepository {
+import com.example.demo.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository <User, Integer> {
+
+
 }
