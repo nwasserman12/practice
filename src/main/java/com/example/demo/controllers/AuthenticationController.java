c54eca46003936112992aeeb68ff9e8ca5302d48
nwasserman12 <nickyann0823@aol.com>
2021-11-23 13:56:10 -0600

    adding registration form

---
+++ src/main/java/com/example/demo/controllers/AuthenticationController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import com.example.demo.data.UserRepository;
+import com.example.demo.models.User;
+import com.example.demo.models.dto.RegisterFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session){
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null){
+            return null;
+        }
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()){
+            return null;
+        }
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user){
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model){
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+}
